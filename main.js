c3f740430e1ae78a292ee99d4c740fbb9986f4b1
Student <student@RU-001.local>
2015-09-07 16:49:19 -0600

    first comment

---
+++ main.js
@@ -0,0 +1,12 @@
+var userInput = prompt("Type a word")
+
+alert(userInput.length + '\n'
+	+ userInput.toUpperCase() + '\n'
+	+ userInput.charAt(4) + '\n'
+	+ userInput.toLowercase() + '\n'
+	+userInput.substring(1,3))
+
+
+
+
+	)
